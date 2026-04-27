ef03f1f5021f2769b6408b5141713a47fbcd54cf
Thomas D <thomas.debuigne@student.junia.com>
2026-04-27 17:30:21 +0200

    resolution bugs

--- js/index.js
+++ js/index.js
@@ -1,34 +1,44 @@
 console.log("JS CHARGE")
 
+
 let slideIndex = 1;
+
 
 document.addEventListener("DOMContentLoaded", () => {
     showSlides(slideIndex);
 });
 
+
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
+
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
+
 function showSlides(n) {
     let slides = document.getElementsByClassName("mySlides");
 
+
     if (slides.length === 0) return;
+
 
     if (n > slides.length) slideIndex = 1;
     if (n < 1) slideIndex = slides.length;
+
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].className = "mySlides";
     }
 
+
     let current = slideIndex - 1;
     let prev = (current - 1 + slides.length) % slides.length;
     let next = (current + 1) % slides.length;
+
 
     slides[current].classList.add("active");
     slides[prev].classList.add("prevSlide");
